932d8e8b22815e308911b085bbe5cc5d8b83c2bb
Shea McAuley <s179049@students.aie.edu.au>
2018-11-26 14:33:34 -0800

    I don't care about this other stuff I htink

---
+++ Assets/Shea/Scripts/Needs.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Needs : MonoBehaviour {
+
+    
+    public float sleep = 100;
+    public float food = 100;
+    public float health = 100;
+
+    private void Update()
+    {
+        sleep -= Time.deltaTime;
+        food -= Time.deltaTime;
+        health -= Time.deltaTime;
+    }
+}
