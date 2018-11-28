f0daed8f6a5e1950d7bc92d5660973ccfba2c38e
Zach Oriel <Zacharyaaron15@gmail.com>
2018-11-27 17:35:34 -0800

    Implemented inventory system, polished up the build manager a bit, and added a storage unit for building

--- Assets/Zach/Scripts/Construction/ConstructionManager.cs
+++ Assets/Zach/Scripts/Construction/ConstructionManager.cs
@@ -3,147 +3,237 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+[System.Serializable]
+public struct ConstructionData
+{
+    [Header("Components and Dependencies")]
+    public Inventory inventory;
+    public ThirdPersonCamera cam;
+    public Transform player;
+    public Transform buildPos;
+
+    [Header("Building Conditions")]
+    public Collider[] blockers;
+    public LayerMask buildMask;
+    [HideInInspector] public bool blueprinted;
+    [HideInInspector] public bool constPanelOpen;
+
+    [Header("Input")]
+    public KeyCode constructionButton;
+    [HideInInspector] public KeyCode defaultConstButton;
+
+    [Header("Buildings")]
+    public GameObject[] transparentBuilding;
+    public GameObject[] builtBuilding;
+    [HideInInspector] public GameObject buildingBlueprint;
+    [HideInInspector] public int buildingNum;
+
+    [Header("UI")]
+    public GameObject constructionPanel;
+    public GameObject[] notEnoughScraps;
+    public Button[] buildingButtons;
+
+    [Header("Settings")]
+    public float buildDistance;
+    public float overlapDistance;
+    public int costOfTurrets;
+    public int costOfStorage;
+
+}
+
 public class ConstructionManager : MonoBehaviour
 {
-    Inventory inventory;
-    ThirdPersonCamera cam;
-
-    public KeyCode constructionButton;
-    public GameObject constructionPanel;
-    public GameObject notEnoughScraps;
-    public GameObject transparentTurret, builtTurret;
-    public Button turretButton; // ADD SHIP PART BUTTONS FOR BUILDING YOUR SHIP
-    public Transform player, buildPos;
-    public LayerMask buildMask;
-    [SerializeField] float buildDistance = 5f;
-    [SerializeField] float overlapDistance = 1f;
-    GameObject turretBlueprint;
-    Collider[] blockers;
-    KeyCode defaultConstButton = KeyCode.Tab;
-    bool constPanelOpen, blueprinted;
+    public ConstructionData data;
 
 	// Use this for initialization
 	void Start ()
     {
         FindComponents();
-        SetButton();
+        SetVariables();
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-		if (Input.GetKeyDown(constructionButton) && !constPanelOpen)
+		if (Input.GetKeyDown(data.constructionButton) && !data.constPanelOpen && !data.blueprinted)
         {
             OpenConstructionPanel();
         }
-        else if (Input.GetKeyDown(constructionButton) && constPanelOpen)
+        else if (Input.GetKeyDown(data.constructionButton) && data.constPanelOpen)
         {
             CloseConstructionPanel();
         }
 
-        if (blueprinted)
+        // If a blueprint is active
+        if (data.blueprinted)
         {
+            // Used to prevent building ontop of other objects
             #region Build Condition
             bool canBuildHere;
-            blockers = Physics.OverlapSphere(buildPos.transform.position, overlapDistance, buildMask, QueryTriggerInteraction.Collide);
+            data.blockers = Physics.OverlapSphere(data.buildPos.transform.position, data.overlapDistance, data.buildMask, QueryTriggerInteraction.Collide);
 
-            if (blockers.Length == 0)
+            // If there are no objects in the location...
+            if (data.blockers.Length == 0)
             {
+                // Player can build there!
                 canBuildHere = true;
             }
+            // Otherwise...
             else
             {
+                // They can't
                 canBuildHere = false;
             }
             #endregion
 
-            // Cancelling the build
+            // If blueprint is active and player presses escape...
             if (Input.GetKeyDown(KeyCode.Escape))
             {
+                // Cancels the blueprint
                 DestroyBlueprint();
             }
 
-            // Build the object
+            // If a blueprint is active and the player clicks...
             if (Input.GetButtonDown("Fire1"))
             {
+                // If it's a valid build location...
                 if (canBuildHere)
                 {
-                    BuildTurret();
+                    if (data.buildingNum == 1)
+                    {
+                        // Builds the turret
+                        BuildTurret();
+                    }
+                    else if (data.buildingNum == 2)
+                    {
+                        // Builds the storage
+                        BuildStorage();
+                    }
                 }
                 else
                 {
-                    // Do something
+                    // Error message/noise/effect
                 }
             }
         }
 	}
 
+    // Opens the build panel
     void OpenConstructionPanel()
     {
-        constPanelOpen = true;
-        constructionPanel.SetActive(true);
-        cam.UnlockCursor();
-        cam.camData.mouseSensitivity = 0f;
+        data.constPanelOpen = true;
+        data.constructionPanel.SetActive(true);
+        data.cam.UnlockCursor();
+        data.cam.camData.mouseSensitivity = 0f;
 
-        if (inventory.numberOfScraps == 0)
+        #region Turret Resource Requirement
+        if (data.inventory.numberOfScraps < data.costOfTurrets)
         {
-            turretButton.interactable = false;
-            notEnoughScraps.SetActive(true);
+            data.buildingButtons[0].interactable = false;
+            data.notEnoughScraps[0].SetActive(true);
         }
         else
         {
-            turretButton.interactable = true;
-            notEnoughScraps.SetActive(false);
+            data.buildingButtons[0].interactable = true;
+            data.notEnoughScraps[0].SetActive(false);
         }
+        #endregion
+
+        #region Storage Resource Requirement
+        if (data.inventory.numberOfScraps < data.costOfStorage)
+        {
+            data.buildingButtons[1].interactable = false;
+            data.notEnoughScraps[1].SetActive(true);
+        }
+        else
+        {
+            data.buildingButtons[1].interactable = true;
+            data.notEnoughScraps[1].SetActive(false);
+        }
+        #endregion
     }
 
+    // Closes the build panel
     void CloseConstructionPanel()
     {
-        constPanelOpen = false;
-        constructionPanel.SetActive(false);
-        cam.LockCursor();
-        cam.camData.mouseSensitivity = 10f;
+        data.constPanelOpen = false;
+        data.constructionPanel.SetActive(false);
+        data.cam.LockCursor();
+        data.cam.camData.mouseSensitivity = 10f;
     }
 
+    // Selects turret for construction, spawns the blueprint 
     public void SelectTurret()
     {
         CloseConstructionPanel();
-        blueprinted = true;
+        data.blueprinted = true;
+        data.buildingNum = 1;
 
-        Vector3 pos = buildPos.transform.position;
-        turretBlueprint = Instantiate(transparentTurret, pos, Quaternion.identity, player.transform);
+        Vector3 pos = data.buildPos.transform.position;
+        data.buildingBlueprint = Instantiate(data.transparentBuilding[0], pos, Quaternion.identity, data.player.transform);
     }
 
+    // Selects storage for construction, spawns the blueprint
+    public void SelectStorage()
+    {
+        CloseConstructionPanel();
+        data.blueprinted = true;
+        data.buildingNum = 2;
+
+        Vector3 pos = data.buildPos.transform.position;
+        data.buildingBlueprint = Instantiate(data.transparentBuilding[1], pos, Quaternion.identity, data.player.transform);
+    }
+
+    // Spawns turret at set location
     void BuildTurret()
     {
-        inventory.numberOfScraps--;
+        data.inventory.numberOfScraps -= data.costOfTurrets;
 
-        Destroy(turretBlueprint);
-        blueprinted = false;
+        Destroy(data.buildingBlueprint);
+        data.blueprinted = false;
 
-        Vector3 pos = buildPos.transform.position;
-        GameObject turret = Instantiate(builtTurret, pos, Quaternion.identity);
+        Vector3 pos = data.buildPos.transform.position;
+        GameObject turret = Instantiate(data.builtBuilding[0], pos, Quaternion.identity);
     }
 
+    // Spawns storage at set location
+    void BuildStorage()
+    {
+        data.inventory.numberOfScraps -= data.costOfStorage;
+
+        Destroy(data.buildingBlueprint);
+        data.blueprinted = false;
+
+        Vector3 pos = data.buildPos.transform.position;
+        GameObject storage = Instantiate(data.builtBuilding[1], pos, Quaternion.identity);
+    }
+
+    // Destroys the blueprint building if player cancels construction
     void DestroyBlueprint()
     {
-        Destroy(turretBlueprint);
-        blueprinted = false;
+        Destroy(data.buildingBlueprint);
+        data.blueprinted = false;
     }
 
     #region Start Functions
     void FindComponents()
     {
-        inventory = FindObjectOfType<Inventory>();
-        cam = FindObjectOfType<ThirdPersonCamera>();
+        data.inventory = FindObjectOfType<Inventory>();
+        data.cam = FindObjectOfType<ThirdPersonCamera>();
     }
 
-    void SetButton()
+    void SetVariables()
     {
-        if (constructionButton == KeyCode.None)
+        data.defaultConstButton = KeyCode.Tab;
+        if (data.constructionButton == KeyCode.None)
         {
-            constructionButton = defaultConstButton;
+            data.constructionButton = data.defaultConstButton;
         }
+
+        data.buildDistance = 5f;
+        data.overlapDistance = 1f;
+        data.costOfTurrets = 1;
+        data.costOfStorage = 5;
     }
     #endregion
 }
