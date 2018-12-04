8c32a00a9fbf1d5e17ace1c2bb9a37b2b4e9f1bd
Shea McAuley <s179049@students.aie.edu.au>
2018-12-04 11:59:56 -0800

    Zombie prefab for zach with animations

---
+++ Assets/Scripts/Ai_Behaviors.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum FoodType { Carnivore, Herbivore, Omnivore }
+public enum MoodType { Passive, Neutral, Aggressive }
+
+public class Ai_Behaviors : MonoBehaviour {
+
+    delegate void AiBehavior();
+    AiBehavior aiBehavior;
+
+    [Header("Statistics")]
+
+    [SerializeField] float sleep_Max = 100;
+    [SerializeField] float food_Max = 100;
+    [SerializeField] float health_Max = 100;
+    [SerializeField] float speed_Max = 5;
+
+    [Space]
+    [Header("Behavior Types")]
+    [SerializeField] FoodType foodType;
+    [SerializeField] MoodType moodType;
+
+    private float cur_Sleep;
+    private float cur_Food;
+    private float cur_Health;
+
+    private void Start()
+    {
+        cur_Food = food_Max;
+        cur_Health = health_Max;
+        cur_Sleep = sleep_Max;
+
+
+        switch(foodType)
+        {
+            case FoodType.Carnivore:
+                aiBehavior += Behavior_Carnivore;
+                break;
+            case FoodType.Herbivore:
+                aiBehavior += Behavior_Herbivore;
+                break;
+            case FoodType.Omnivore:
+                aiBehavior += Behavior_Omnivore;
+                break;
+        }
+        switch (moodType)
+        {
+            case MoodType.Aggressive:
+                aiBehavior += Behavior_Aggressive;
+                break;
+            case MoodType.Neutral:
+                aiBehavior += Behavior_Neutral;
+                break;
+            case MoodType.Passive:
+                aiBehavior += Behavior_Passive;
+                break;
+        }
+        aiBehavior();
+    }
+
+    public void Behavior_Carnivore()
+    {
+
+        Debug.Log("I eat meat");
+    }
+    public void Behavior_Herbivore()
+    {
+        Debug.Log("I eat plants");
+    }
+    public void Behavior_Omnivore()
+    {
+        Debug.Log("I eat anything");
+    }
+    public void Behavior_Passive()
+    {
+        Debug.Log("I won't attack back");
+    }
+    public void Behavior_Neutral()
+    {
+        Debug.Log("I will defend myself");
+    }
+    public void Behavior_Aggressive()
+    {
+        Debug.Log("I will attack on sight");
+    }
+}
---
+++ Assets/Scripts/ZombieFlocking.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ZombieFlocking : MonoBehaviour {
+
+    [SerializeField] Animator anim;
+
+    [SerializeField] float searchRadius;
+    [SerializeField] LayerMask searchForLayer;
+    
+    
+    [SerializeField] float updateNewPosition_Timer = 10;
+    [SerializeField] float updateNewPosition_Radius = 25;
+
+    float updatePositionTimer = 0;
+
+    bool updateNewPosition = true;
+
+    NavMeshAgent agent;
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+
+        Collider[] hitObjects = Physics.OverlapSphere(transform.position, searchRadius, searchForLayer, QueryTriggerInteraction.Ignore);
+        agent.speed = 1.5f;
+        foreach (Collider n in hitObjects)
+        {
+            
+            if (n.CompareTag("Player"))
+            {
+                agent.speed = 7f;
+                updateNewPosition = false;
+                agent.SetDestination(n.gameObject.transform.position);
+            }
+
+           
+
+            Debug.Log(n);
+        }
+
+        Vector3 point;
+
+        if(updateNewPosition)
+        {
+            if (RandomPoint(transform.position, searchRadius, out point))
+            {
+                agent.SetDestination(point);
+                Debug.DrawRay(point, Vector3.up, Color.blue, 1.0f);
+                updatePositionTimer = agent.remainingDistance + updateNewPosition_Timer;
+                if(agent.remainingDistance != 0)
+                {
+                    Debug.Log(agent.remainingDistance);
+                }
+                updateNewPosition = false;
+            }
+            else
+            {
+                updateNewPosition = true;
+            }
+        }
+        if (updatePositionTimer > 0)
+        {
+            updatePositionTimer -= Time.deltaTime;
+        }
+        else
+        {
+            updateNewPosition = true;
+        }
+        float speedPercent = agent.velocity.magnitude / agent.speed;
+        anim.SetFloat("speedPercent", speedPercent, .1f,Time.deltaTime); 
+        
+    }
+
+    bool RandomPoint(Vector3 center, float range, out Vector3 result)
+    {
+        Vector3 randomPoint = center + (Random.insideUnitSphere  * range) + (Vector3.one * range/4);
+        NavMeshHit hit;
+        if (NavMesh.SamplePosition(randomPoint, out hit, 1.0f, NavMesh.AllAreas))
+        {
+            result = hit.position;
+            return true;
+        }
+        
+        result = Vector3.zero;
+        return false;
+    }
+
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position, updateNewPosition_Radius);
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, searchRadius);
+    }
+
+}
