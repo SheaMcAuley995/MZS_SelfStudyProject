a438d4b7d23346d1d15e4a037b4d6aaca538a29a
Shea McAuley <s179049@students.aie.edu.au>
2018-12-04 17:16:11 -0800

    SpawnPoint Generator

---
+++ Assets/Scripts/SpawnPointNode.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPointNode{
+
+    public bool spawnable;
+    public Vector3 worldPosition;
+
+    public SpawnPointNode(bool _Spawnable, Vector3 _worldPos)
+    {
+        spawnable = _Spawnable;
+        worldPosition = _worldPos;
+    }
+
+}
---
+++ Assets/Scripts/SpawnPointsOnMesh.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPointsOnMesh : MonoBehaviour {
+
+    [SerializeField] float spawnPointHieght;
+    public SpawnPointNode[] spawnGrid;
+    public MeshFilter filter;
+    
+
+    private void Start()
+    {  
+        filter = GetComponent<MeshFilter>();
+        spawnGrid = new SpawnPointNode[filter.mesh.vertexCount + 1];
+        spawnNodesOnMesh(filter.mesh);
+    }
+
+
+    public void spawnNodesOnMesh(Mesh mesh)
+    {
+        for(int i = 0; i < mesh.vertexCount; i++)
+        {
+            Vector3 worldSpacePos = transform.TransformPoint(new Vector3(mesh.vertices[i].x, mesh.vertices[i].y, mesh.vertices[i].z));
+           bool isSpawnable = (worldSpacePos.y > spawnPointHieght);
+           spawnGrid[i] = new SpawnPointNode(isSpawnable, worldSpacePos);
+           Debug.Log(worldSpacePos);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        foreach(SpawnPointNode n in spawnGrid)
+        {
+            if(!n.spawnable)
+            {
+                Gizmos.color = Color.red;
+            }
+            else
+            {
+                Gizmos.color = Color.green;
+            }
+            
+            Gizmos.DrawSphere(n.worldPosition, 0.1f);
+        }
+    }
+}
