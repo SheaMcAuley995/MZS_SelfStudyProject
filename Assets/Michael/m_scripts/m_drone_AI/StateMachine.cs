fbc5a6d6f110afecf1ebd23bfc8d70bc1d6f61f0
Michael Kaplor <mkaplor@gmail.com>
2018-12-11 12:49:45 -0800

    added drone ai

---
+++ Assets/Michael/m_scripts/m_drone_AI/StateMachine.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum States { Wonder, Fetch, Return}
+public class StateMachine : MonoBehaviour {
+
+    States state = States.Wonder;
+    DroneCollection_AI drone;
+
+    private void Start()
+    {
+        drone = GetComponent<DroneCollection_AI>();
+    }
+    private void Update()
+    {
+
+
+
+
+        switch (state)
+        {
+            case States.Wonder:
+                Wonder();
+                break;
+            case States.Fetch:
+                Fetch();
+                break;
+            case States.Return:
+                Return();
+                break;
+        }
+
+    }
+
+    void Wonder()
+    {
+        drone.Wander();
+        //finds scarap then state = States.Fetch;
+    }
+
+    void Fetch()
+    {
+
+    }
+
+    void Return()
+    {
+
+    }
+}
