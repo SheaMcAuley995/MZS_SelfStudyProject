a39402564bac83acf7378d08ce5ff2d1fb1db479
Zach Oriel <Zacharyaaron15@gmail.com>
2018-12-10 17:56:34 -0800

    Worked on factory

--- Assets/Zach/Scripts/Management/Factory.cs
+++ Assets/Zach/Scripts/Management/Factory.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Factory : MonoBehaviour
 {
@@ -21,17 +22,54 @@ public class Factory : MonoBehaviour
     }
     #endregion
 
+    [Header("Dependencies")]
     ThirdPersonController controller;
     ThirdPersonCamera cam;
+    public GameObject factoryPanel;
     [HideInInspector] public bool inspectingFactory;
+
+    Color yellow = Color.yellow;
+
+    void Start()
+    {
+        FindObjects();
+    }
 
     public void OpenFactoryUI()
     {
+        factoryPanel.SetActive(true);
+        inspectingFactory = true;
 
+        DisableControlls();
     }
 
     public void CloseFactoryUI()
     {
+        factoryPanel.SetActive(false);
+        inspectingFactory = false;
 
+        ResetControlls();
+        FactoryUI.instance.ResetUI();
+    }
+
+    void DisableControlls()
+    {
+        controller.enabled = false;
+        cam.UnlockCursor();
+        cam.camData.mouseSensitivity = 0f;
+    }
+
+    void ResetControlls()
+    {
+        controller.enabled = true;
+        cam.LockCursor();
+        cam.camData.mouseSensitivity = 10f;
+    }
+
+    void FindObjects()
+    {
+        controller = FindObjectOfType<ThirdPersonController>();
+        cam = FindObjectOfType<ThirdPersonCamera>();
+        factoryPanel = GameObject.FindGameObjectWithTag("Factory Panel");
     }
 }
